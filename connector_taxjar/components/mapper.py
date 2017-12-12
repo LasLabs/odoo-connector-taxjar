50ebcf713e5edc440fb3f48da39a616e7fc3baf5
Dave Lasley <dave@laslabs.com>
2017-12-12 12:34:57 -0800

    [ADD] connector_taxjar: TaxJar connector

---
+++ connector_taxjar/components/mapper.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.addons.component.core import AbstractComponent
+from odoo.addons.connector.components.mapper import mapping, only_create
+
+
+class TaxjarImportMapper(AbstractComponent):
+    _name = 'taxjar.import.mapper'
+    _inherit = ['base.taxjar.connector', 'base.import.mapper']
+    _usage = 'import.mapper'
+
+    @mapping
+    @only_create
+    def backend_id(self, _):
+        return {'backend_id': self.backend_record.id}
+
+
+class TaxjarExportMapper(AbstractComponent):
+    _name = 'taxjar.export.mapper'
+    _inherit = ['base.taxjar.connector', 'base.export.mapper']
+    _usage = 'export.mapper'
---
+++ connector_taxjar/models/account_fiscal_position/importer.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.addons.component.core import Component
+from odoo.addons.connector.components.mapper import mapping, only_create
+
+
+class TaxjarAccountFiscalPositionImportMapper(Component):
+    _name = 'taxjar.import.mapper.account.fiscal.position'
+    _inherit = 'taxjar.import.mapper'
+    _apply_on = 'taxjar.account.fiscal.position'
+
+    @mapping
+    def name(self, record):
+        return {
+            'name': '[%s] %s, %s' % (
+                self.backend_record.name,
+                record['region'],
+                record['region_code'],
+            )
+        }
+
+    @mapping
+    def country_id_state_ids(self, record):
+        country = self.env['res.country'].search([
+            ('code', '=', record['country_code']),
+        ])
+        state = self.env['res.country.state'].search([
+            ('country_id', '=', country.id),
+            ('code', '=', record['region_code']),
+        ])
+        return {
+            'country_id': country.id,
+            'state_ids': [(6, 0, state.ids)],
+        }
+
+    @mapping
+    @only_create
+    def static_mappings(self, _):
+        """Return the staticly defined values."""
+        return {'auto_apply': True,
+                }
+
+
+class TaxjarAccountFiscalPositionImporter(Component):
+    """Import one Taxjar record."""
+    _name = 'taxjar.record.importer.account.fiscal.position'
+    _inherit = 'taxjar.importer'
+    _apply_on = 'taxjar.account.fiscal.position'
