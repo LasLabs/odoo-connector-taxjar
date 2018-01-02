44e579678fec1ccaa89fb356ea2807d5046a24c3
Dave Lasley <dave@laslabs.com>
2018-01-02 13:23:48 -0800

    [MIG] purchase_tax_connector: Quick upgrade to v11

--- purchase_tax_connector/__manifest__.py
+++ purchase_tax_connector/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 LasLabs Inc.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
@@ -6,7 +5,7 @@
 {
     "name": "Purchase Tax Connector",
     "summary": "Tax connector adapter for purchasing.",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Connector",
     "website": "https://laslabs.com",
     "author": "LasLabs",
